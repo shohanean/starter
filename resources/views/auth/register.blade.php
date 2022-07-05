58e5a3cfbe1694f88e12d3e3aa75e99000415e7c
shohanean <hossain_shohan1@live.com>
2022-07-06 00:03:59 +0600

    register form workable

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -33,8 +33,9 @@
 					<!--begin::Wrapper-->
 					<div class="w-lg-600px bg-body rounded shadow-sm p-10 p-lg-15 mx-auto">
 						<!--begin::Form-->
-						<form class="form w-100" novalidate="novalidate" id="kt_sign_up_form">
-							<!--begin::Heading-->
+						<form class="form w-100" method="POST" action="{{ route('register') }}">
+							@csrf
+                            <!--begin::Heading-->
 							<div class="mb-10 text-center">
 								<!--begin::Title-->
 								<h1 class="text-dark mb-3">Create an Account</h1>
@@ -57,25 +58,21 @@
 							</div>
 							<!--end::Separator-->
 							<!--begin::Input group-->
-							<div class="row fv-row mb-7">
-								<!--begin::Col-->
-								<div class="col-xl-6">
-									<label class="form-label fw-bolder text-dark fs-6">First Name</label>
-									<input class="form-control form-control-lg form-control-solid" type="text" placeholder="" name="first-name" autocomplete="off" />
-								</div>
-								<!--end::Col-->
-								<!--begin::Col-->
-								<div class="col-xl-6">
-									<label class="form-label fw-bolder text-dark fs-6">Last Name</label>
-									<input class="form-control form-control-lg form-control-solid" type="text" placeholder="" name="last-name" autocomplete="off" />
-								</div>
-								<!--end::Col-->
+							<div class="fv-row mb-7">
+								<label class="form-label fw-bolder text-dark fs-6">Name</label>
+								<input class="@error('name') is-invalid @enderror form-control form-control-lg form-control-solid" type="text" placeholder="" name="name" autocomplete="off" value="{{ old('name') }}"/>
+                                @error('name')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
 							</div>
 							<!--end::Input group-->
 							<!--begin::Input group-->
 							<div class="fv-row mb-7">
 								<label class="form-label fw-bolder text-dark fs-6">Email</label>
-								<input class="form-control form-control-lg form-control-solid" type="email" placeholder="" name="email" autocomplete="off" />
+								<input class="@error('email') is-invalid @enderror form-control form-control-lg form-control-solid" type="email" placeholder="" name="email" autocomplete="off" value="{{ old('email') }}"/>
+                                @error('email')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
 							</div>
 							<!--end::Input group-->
 							<!--begin::Input group-->
@@ -92,41 +89,24 @@
 											<i class="bi bi-eye-slash fs-2"></i>
 											<i class="bi bi-eye fs-2 d-none"></i>
 										</span>
+                                        @error('password')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
 									</div>
 									<!--end::Input wrapper-->
-									<!--begin::Meter-->
-									<div class="d-flex align-items-center mb-3" data-kt-password-meter-control="highlight">
-										<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
-										<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
-										<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
-										<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px"></div>
-									</div>
-									<!--end::Meter-->
 								</div>
 								<!--end::Wrapper-->
-								<!--begin::Hint-->
-								<div class="text-muted">Use 8 or more characters with a mix of letters, numbers &amp; symbols.</div>
-								<!--end::Hint-->
 							</div>
 							<!--end::Input group=-->
 							<!--begin::Input group-->
 							<div class="fv-row mb-5">
 								<label class="form-label fw-bolder text-dark fs-6">Confirm Password</label>
-								<input class="form-control form-control-lg form-control-solid" type="password" placeholder="" name="confirm-password" autocomplete="off" />
-							</div>
-							<!--end::Input group-->
-							<!--begin::Input group-->
-							<div class="fv-row mb-10">
-								<label class="form-check form-check-custom form-check-solid form-check-inline">
-									<input class="form-check-input" type="checkbox" name="toc" value="1" />
-									<span class="form-check-label fw-bold text-gray-700 fs-6">I Agree
-									<a href="#" class="ms-1 link-primary">Terms and conditions</a>.</span>
-								</label>
+								<input class="form-control form-control-lg form-control-solid" type="password" placeholder="" name="password_confirmation" autocomplete="off" />
 							</div>
 							<!--end::Input group-->
 							<!--begin::Actions-->
 							<div class="text-center">
-								<button type="button" id="kt_sign_up_submit" class="btn btn-lg btn-primary">
+								<button type="submit" class="btn btn-lg btn-primary">
 									<span class="indicator-label">Submit</span>
 									<span class="indicator-progress">Please wait...
 									<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
