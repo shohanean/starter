26af68118752eb39b5f06489e77ca4e9a65b715a
Md. Shohan Hossain Ean <hossain_shohan1@live.com>
2022-07-24 17:33:07 +0600

    excel install not working

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -368,17 +368,10 @@
                     <span class="card-label fw-bolder fs-3 mb-1">Members Statistics</span>
                     <span class="text-muted mt-1 fw-bold fs-7">Over 500 members</span>
                 </h3>
-                <div class="card-toolbar" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover" title="" data-bs-original-title="Click to add a user">
-                    <a href="#" class="btn btn-sm btn-light btn-active-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_invite_friends">
-                    <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
-                    <span class="svg-icon svg-icon-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <rect opacity="0.5" x="11.364" y="20.364" width="16" height="2" rx="1" transform="rotate(-90 11.364 20.364)" fill="currentColor"></rect>
-                            <rect x="4.36396" y="11.364" width="16" height="2" rx="1" fill="currentColor"></rect>
-                        </svg>
-                    </span>
-                    <!--end::Svg Icon-->New Member</a>
-                </div>
+                <form action="">
+                    <input type="file" class="form-control form-control-sm" id="floatingInput" placeholder="name@example.com"/>
+                    <button class="btn btn-success btn-sm">Upload</button>
+                </form>
             </div>
             <!--end::Header-->
             <!--begin::Body-->
