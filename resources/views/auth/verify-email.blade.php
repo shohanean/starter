b4ddc16004a02fc1d56e81fc73723cdf9569e7f2
Md. Shohan Hossain Ean <hossain_shohan1@live.com>
2022-07-24 12:07:15 +0600

    verify email done

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,100 +1,71 @@
-<!DOCTYPE html>
-<html lang="en">
-	<!--begin::Head-->
-	<head><base href="../../">
-		<title>Metronic - the world's #1 selling Bootstrap Admin Theme Ecosystem for HTML, Vue, React, Angular &amp; Laravel by Keenthemes</title>
-		<meta charset="utf-8" />
-		<meta name="description" content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Blazor, Django, Flask &amp; Laravel versions. Grab your copy now and get life-time updates for free." />
-		<meta name="keywords" content="Metronic, Bootstrap, Bootstrap 5, Angular, VueJs, React, Asp.Net Core, Blazor, Django, Flask &amp; Laravel, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta property="og:locale" content="en_US" />
-		<meta property="og:type" content="article" />
-		<meta property="og:title" content="Metronic - Bootstrap 5 HTML, VueJS, React, Angular, Asp.Net Core, Blazor, Django, Flask &amp; Laravel Admin Dashboard Theme" />
-		<meta property="og:url" content="https://keenthemes.com/metronic" />
-		<meta property="og:site_name" content="Keenthemes | Metronic" />
-		<link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
-		<link rel="shortcut icon" href="{{ asset('dashboard_assets') }}/media/logos/favicon.ico" />
-		<!--begin::Fonts-->
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
-		<!--end::Fonts-->
-		<!--begin::Global Stylesheets Bundle(used by all pages)-->
-		<link href="{{ asset('dashboard_assets') }}/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
-		<link href="{{ asset('dashboard_assets') }}/css/style.bundle.css" rel="stylesheet" type="text/css" />
-		<!--end::Global Stylesheets Bundle-->
-	</head>
-	<!--end::Head-->
-	<!--begin::Body-->
-	<body data-kt-name="metronic" id="kt_body" class="app-blank app-blank bgi-size-cover bgi-position-center bgi-no-repeat">
-		<!--begin::Root-->
-		<div class="d-flex flex-column flex-root" id="kt_app_root">
-			<!--begin::Page bg image-->
-			<style>body { background-image: url('{{ asset('dashboard_assets') }}/media/auth/bg5.jpg'); } [data-theme="dark"] body { background-image: url('{{ asset('dashboard_assets') }}/media/auth/bg5-dark.jpg'); }</style>
-			<!--end::Page bg image-->
-			<!--begin::Authentication - Signup Welcome Message -->
-			<div class="d-flex flex-column flex-center flex-column-fluid">
-				<!--begin::Content-->
-				<div class="d-flex flex-column flex-center text-center p-10">
-					<!--begin::Wrapper-->
-					<div class="card card-flush w-lg-650px py-5">
-						<div class="card-body py-15 py-lg-20">
-							<!--begin::Logo-->
-							<div class="mb-14">
-								<a href="{{ route('index') }}" class="">
-									<img alt="Logo" src="{{ env('PROJECT_LOGO') }}" class="h-40px" />
-								</a>
-							</div>
-							<!--end::Logo-->
+@extends('layouts.auth')
 
-							<!--begin::Title-->
-							<h1 class="fw-bolder text-gray-900 mb-5">Verify your email</h1>
-							<!--end::Title-->
-							<!--begin::Action-->
-							<div class="fs-6 mb-8">
-								<span class="fw-semibold text-gray-500">Did’t receive an email?</span>
-							</div>
-							<!--end::Action-->
-                            @if (session('message'))
-                            <div class="alert alert-primary d-flex align-items-center p-5 mb-10">
-                                <!--begin::Svg Icon | path: icons/duotune/general/gen048.svg-->
-                                <span class="svg-icon svg-icon-2hx svg-icon-primary me-4">
-                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path opacity="0.3" d="M20.5543 4.37824L12.1798 2.02473C12.0626 1.99176 11.9376 1.99176 11.8203 2.02473L3.44572 4.37824C3.18118 4.45258 3 4.6807 3 4.93945V13.569C3 14.6914 3.48509 15.8404 4.4417 16.984C5.17231 17.8575 6.18314 18.7345 7.446 19.5909C9.56752 21.0295 11.6566 21.912 11.7445 21.9488C11.8258 21.9829 11.9129 22 12.0001 22C12.0872 22 12.1744 21.983 12.2557 21.9488C12.3435 21.912 14.4326 21.0295 16.5541 19.5909C17.8169 18.7345 18.8277 17.8575 19.5584 16.984C20.515 15.8404 21 14.6914 21 13.569V4.93945C21 4.6807 20.8189 4.45258 20.5543 4.37824Z" fill="currentColor"></path>
-                                        <path d="M10.5606 11.3042L9.57283 10.3018C9.28174 10.0065 8.80522 10.0065 8.51412 10.3018C8.22897 10.5912 8.22897 11.0559 8.51412 11.3452L10.4182 13.2773C10.8099 13.6747 11.451 13.6747 11.8427 13.2773L15.4859 9.58051C15.771 9.29117 15.771 8.82648 15.4859 8.53714C15.1948 8.24176 14.7183 8.24176 14.4272 8.53714L11.7002 11.3042C11.3869 11.6221 10.874 11.6221 10.5606 11.3042Z" fill="currentColor"></path>
-                                    </svg>
-                                </span>
-                                <!--end::Svg Icon-->
-                                <div class="d-flex flex-column">
-                                    <h4 class="mb-1 text-primary">Success</h4>
-                                    <span>
-                                        {{ session('message') }}
-                                    </span>
-                                </div>
-                            </div>
-                            @endif
-                            <form action="{{ route('verification.send') }}" method="POST">
-                                @csrf
-                                <button class="btn btn-sm btn-primary" type="submit">Try Again</button>
-                            </form>
-							<!--begin::Illustration-->
-							<div class="mb-0">
-								<img src="{{ asset('dashboard_assets') }}/media/auth/please-verify-your-email.png" class="mw-100 mh-300px theme-light-show" alt="" />
-							</div>
-							<!--end::Illustration-->
-						</div>
-					</div>
-					<!--end::Wrapper-->
-				</div>
-				<!--end::Content-->
-			</div>
-			<!--end::Authentication - Signup Welcome Message-->
-		</div>
-		<!--end::Root-->
-		<!--begin::Javascript-->
-		<script src="{{ asset('dashboard_assets') }}/plugins/global/plugins.bundle.js"></script>
-		<script src="{{ asset('dashboard_assets') }}/js/scripts.bundle.js"></script>
-		<!--end::Global Javascript Bundle-->
-		<!--end::Javascript-->
-	</body>
-	<!--end::Body-->
-</html>
+@section('content')
+<!--begin::Authentication - Signup Welcome Message -->
+<div class="d-flex flex-column flex-center flex-column-fluid">
+    <!--begin::Content-->
+    <div class="d-flex flex-column flex-center text-center p-10">
+        <!--begin::Wrapper-->
+        <div class="card-body py-1 py-lg-1">
 
+            @empty(auth()->user()->email_verified_at)
+                    <!--begin::Title-->
+                    <h1 class="fw-bolder text-gray-900 mb-5">Verify your email</h1>
+                    <!--end::Title-->
+                    <!--begin::Action-->
+                    <div class="fs-6 mb-8">
+                        <span class="fw-semibold text-gray-500">Did’t receive an email?</span>
+                    </div>
+                    <!--end::Action-->
+                    @if (session('message'))
+                    <div class="alert alert-primary d-flex align-items-center p-5 mb-10">
+                        <!--begin::Svg Icon | path: icons/duotune/general/gen048.svg-->
+                        <span class="svg-icon svg-icon-2hx svg-icon-primary me-4">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.3" d="M20.5543 4.37824L12.1798 2.02473C12.0626 1.99176 11.9376 1.99176 11.8203 2.02473L3.44572 4.37824C3.18118 4.45258 3 4.6807 3 4.93945V13.569C3 14.6914 3.48509 15.8404 4.4417 16.984C5.17231 17.8575 6.18314 18.7345 7.446 19.5909C9.56752 21.0295 11.6566 21.912 11.7445 21.9488C11.8258 21.9829 11.9129 22 12.0001 22C12.0872 22 12.1744 21.983 12.2557 21.9488C12.3435 21.912 14.4326 21.0295 16.5541 19.5909C17.8169 18.7345 18.8277 17.8575 19.5584 16.984C20.515 15.8404 21 14.6914 21 13.569V4.93945C21 4.6807 20.8189 4.45258 20.5543 4.37824Z" fill="currentColor"></path>
+                                <path d="M10.5606 11.3042L9.57283 10.3018C9.28174 10.0065 8.80522 10.0065 8.51412 10.3018C8.22897 10.5912 8.22897 11.0559 8.51412 11.3452L10.4182 13.2773C10.8099 13.6747 11.451 13.6747 11.8427 13.2773L15.4859 9.58051C15.771 9.29117 15.771 8.82648 15.4859 8.53714C15.1948 8.24176 14.7183 8.24176 14.4272 8.53714L11.7002 11.3042C11.3869 11.6221 10.874 11.6221 10.5606 11.3042Z" fill="currentColor"></path>
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->
+                        <div class="d-flex flex-column">
+                            <h4 class="mb-1 text-primary">Success</h4>
+                            <span>
+                                {{ session('message') }}
+                            </span>
+                        </div>
+                    </div>
+                    @endif
+                    <form action="{{ route('verification.send') }}" method="POST">
+                        @csrf
+                        <button class="btn btn-sm btn-primary" type="submit">Try Again</button>
+                    </form>
+                    <!--begin::Illustration-->
+                    <div class="mb-0">
+                        <img src="{{ asset('dashboard_assets') }}/media/auth/please-verify-your-email.png" class="mw-100 mh-300px theme-light-show" alt="" />
+                    </div>
+                    <!--end::Illustration-->
+                @else
+                    <!--begin::Title-->
+                    <h1 class="fw-bolder text-gray-900 mb-10">
+                        {{ auth()->user()->name }}, your email already verified!
+                    </h1>
+                    <!--end::Title-->
+                    <!--begin::Illustration-->
+                    <div class="mb-0">
+                        <span class="svg-icon svg-icon-5hx svg-icon-primary me-4">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.3" d="M20.5543 4.37824L12.1798 2.02473C12.0626 1.99176 11.9376 1.99176 11.8203 2.02473L3.44572 4.37824C3.18118 4.45258 3 4.6807 3 4.93945V13.569C3 14.6914 3.48509 15.8404 4.4417 16.984C5.17231 17.8575 6.18314 18.7345 7.446 19.5909C9.56752 21.0295 11.6566 21.912 11.7445 21.9488C11.8258 21.9829 11.9129 22 12.0001 22C12.0872 22 12.1744 21.983 12.2557 21.9488C12.3435 21.912 14.4326 21.0295 16.5541 19.5909C17.8169 18.7345 18.8277 17.8575 19.5584 16.984C20.515 15.8404 21 14.6914 21 13.569V4.93945C21 4.6807 20.8189 4.45258 20.5543 4.37824Z" fill="currentColor"></path>
+                                <path d="M10.5606 11.3042L9.57283 10.3018C9.28174 10.0065 8.80522 10.0065 8.51412 10.3018C8.22897 10.5912 8.22897 11.0559 8.51412 11.3452L10.4182 13.2773C10.8099 13.6747 11.451 13.6747 11.8427 13.2773L15.4859 9.58051C15.771 9.29117 15.771 8.82648 15.4859 8.53714C15.1948 8.24176 14.7183 8.24176 14.4272 8.53714L11.7002 11.3042C11.3869 11.6221 10.874 11.6221 10.5606 11.3042Z" fill="currentColor"></path>
+                            </svg>
+                        </span>
+                    </div>
+                    <!--end::Illustration-->
+                    <a class="btn btn-sm btn-primary mt-10" href="{{ route('home') }}">Go to Dashboard</a>
+                @endempty
+            </div>
+        <!--end::Wrapper-->
+    </div>
+    <!--end::Content-->
+</div>
+<!--end::Authentication - Signup Welcome Message-->
+@endsection
