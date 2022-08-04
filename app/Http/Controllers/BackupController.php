8f104a45a5ad45753d622445358782329c62bb76
shohanean <hossain_shohan1@live.com>
2022-08-05 01:30:07 +0600

    backup create left

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Storage;
 
 class BackupController extends Controller
 {
@@ -13,7 +16,12 @@ class BackupController extends Controller
      */
     public function index()
     {
-        return view('backend.backup.index');
+        // return collect(Storage::allFiles('db-backup'))->sortKeys();
+        // return Storage::path('db-backup/2022-08-04-17-25-22.zip');
+        // return Storage::download('db-backup/2022-08-04-17-25-22.zip');
+        return view('backend.backup.index',[
+            'backups' => collect(Storage::allFiles('db-backup'))->sortKeysDesc()
+        ]);
     }
 
     /**
@@ -34,7 +42,11 @@ class BackupController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // return Artisan::call("backup:run");
+        // return Artisan::call("backup:run", [
+        //     "--only-db" => true
+        // ]);
+        // return back();
     }
 
     /**
@@ -45,7 +57,7 @@ class BackupController extends Controller
      */
     public function show($id)
     {
-        //
+        return Storage::download(Crypt::decrypt($id));
     }
 
     /**
@@ -79,6 +91,7 @@ class BackupController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Storage::delete(Crypt::decrypt($id));
+        return back();
     }
 }
