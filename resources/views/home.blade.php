996b1e30d60cc51d9e43a923e2f13d7387986c49
Md. Shohan Hossain Ean <hossain_shohan1@live.com>
2022-09-05 14:22:11 +0600

    minor

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -190,7 +190,7 @@
             <!--begin::Header-->
             <div class="card-header border-0 pt-5">
                 <h3 class="card-title align-items-start flex-column">
-                    <span class="card-label fw-bolder fs-3 mb-1">Members Statistics</span>
+                    <span class="card-label fw-bolder fs-3 mb-1">User Statistics</span>
                     <span class="text-muted mt-1 fw-bold fs-7">Over {{ $users->count() }} members</span>
                 </h3>
             </div>
